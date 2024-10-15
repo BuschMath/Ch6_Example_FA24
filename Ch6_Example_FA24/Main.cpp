b467b6d34f617e5dcc97d8ac6db59432a33557b7
BuschMath <busch@iowacentral.edu>
2024-10-15 09:54:29 -0500

    Sentinel

--- Ch6_Example_FA24/Main.cpp
+++ Ch6_Example_FA24/Main.cpp
@@ -1,50 +1,28 @@
 #include <iostream>
-#include <cstdlib>
-#include <time.h>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Number Guessing Game" << endl;
-	cout << "Guess a number between 1 and 100" << endl;
-	cout << "You have 3 chances to guess the number" << endl;
-	int tryCount = 0;
+	char inChar;
+	ifstream inFile;
 
-	srand(time(NULL));		// Seed the random number generator
-	int number = rand() % 100 + 1;
-	int guess = 0;
+	inFile.open("SentinelInputData.dat");
 
-	while (tryCount < 3)
+	if (!inFile)
 	{
-		cout << "Enter your guess: ";
-		cin >> guess;
-		tryCount++;
-
-		if (guess == number)
-		{
-			cout << "Congratulations! You guessed the number!" << endl;
-			break;
-		}
-		else if (guess < number)
-		{
-			cout << "Your guess is too low" << endl;
-		}
-		else
-		{
-			cout << "Your guess is too high" << endl;
-		}
-
-		if (tryCount == 3)
-		{
-			cout << "Sorry, you have used all your chances" << endl;
-			cout << "The number was: " << number << endl;
-		}
-		else
-		{
-			cout << "You have " << 3 - tryCount << " chances left" << endl;
-		}
+		cout << "File failed to open!\n";
+		return 1;
 	}
+
+	inFile.get(inChar);
+	while (inChar != '\n')
+	{
+		cout << inChar << endl;
+		inFile.get(inChar);
+	}
+	cout << endl;
 
 	return 0;
 }
